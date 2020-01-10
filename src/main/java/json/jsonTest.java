12d84690ecd5046b84e17d11347c255e01f90739
WangChen <wangchen@lengint.com>
2020-01-10 17:03:04 +0800

    新增：json多层解析

---
+++ src/main/java/json/jsonTest.java
@@ -0,0 +1,42 @@
+package json;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author WangChen
+ * @filename jsonTest
+ * @date 2020/1/10 15:40
+ * @description Json的学习
+ */
+public class jsonTest {
+    /**
+     * JSONObject和JSONArray的数据表示形式
+     * JSONObject的数据是用 {  } 来表示的，
+     * 例如：   { "id" : "123", "courseID" : "huangt-test", "title" : "提交作业", "content" : null  }  
+     * JSONArray，顾名思义是由JSONObject构成的数组，用  [ { } , { } , ......  , { } ]  来表示
+     * 例如：[ {  "id" : "123", "courseID" : "huangt-test", "title" : "提交作业" }  ,  {  "content" : null, "beginTime" : 1398873600000  "endTime" } ] ； 
+     * 表示了包含2个JSONObject的JSONArray。
+     * 可以看到一个很明显的区别，一个最外面用的是 {  }  ，一个最外面用的是 [  ]  
+     */
+    public static void main(String[] args) throws IOException {
+        File file = new File("e://1.json");
+        String params = null;
+        try {
+            params = FileUtils.readFileToString(file, "utf-8");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        JSONObject jsonObject = JSONObject.parseObject(params);
+        System.out.println(jsonObject);
+        System.out.println(jsonObject.get("face"));
+        JSONArray jsonArray = JSONArray.parseArray(jsonObject.getString("face"));
+        JSONObject object = jsonArray.getJSONObject(0);
+        System.out.println(object);
+        System.out.println(object.getString("face_id"));
+    }
+}
